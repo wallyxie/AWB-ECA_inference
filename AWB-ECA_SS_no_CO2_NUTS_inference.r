5b4fa67adbf2b20f1459e8233d2406890636ddc0
Hua Xie <x@wallyxie.com>
2021-12-23 22:55:22 -0800

    Updated PyStan and CmdStanR scripts for Stan HMC inference following prior changes in data generating process. Updated PyStan and CmdStanR scripts accordingly.

--- AWB-ECA_SS_no_CO2_NUTS_inference.r
+++ AWB-ECA_SS_no_CO2_NUTS_inference.r
@@ -16,8 +16,8 @@ prior_scale_factor <- 0.25
 obs_error_scale <- 0.1
 obs_every <- 10 #Observations every 10 hours.
 t <- 3000 #Total time span of ODE simulation.
-x_hat0 <- c(78.80557523, 2.83158185, 2.22155509, 1.19246832) #Originally sampled values used for Euler-Maruyama solution.
-y_full <- read_csv('generated_data/SAWB-ECA-SS_no_CO2_trunc_short_2021_12_20_18_20_sample_y_t_2000_dt_0-01_sd_scale_0-25.csv')
+x_hat0 <- c(99.69113179, 1.94470102, 1.87967951, 2.01791824) #Originally sampled values used for Euler-Maruyama solution.
+y_full <- read_csv('generated_data/SAWB-ECA-SS_no_CO2_trunc_short_2021_12_23_22_21_sample_y_t_3000_dt_0-01_sd_scale_0-25.csv')
 y <- y_full %>% filter(hour <= t) %>% tail(-1)
 ts <- y$hour
 N_t <- length(ts)
@@ -26,18 +26,18 @@ y <- y %>% select(-hour)
 y <- as.list(y) #Convert data observations to list of columns to correspond to Stan's array of vectors type.
 
 #Parameter prior means
-u_Q_ref_prior_dist_params <- c(0.25, 0, 1)
+u_Q_ref_prior_dist_params <- c(0.22, 0, 1)
 Q_prior_dist_params <- c(0.001, 0, 0.1)
 a_MSA_prior_dist_params <- c(0.5, 0, 1)
-K_DE_prior_dist_params <- c(1000, 0, 5000)
-K_UE_prior_dist_params <- c(0.1, 0, 1)
-V_DE_ref_prior_dist_params <- c(0.11, 0, 1)
-V_UE_ref_prior_dist_params <- c(0.0044, 0, 0.1)
+K_DE_prior_dist_params <- c(1000, 100, 5000)
+K_UE_prior_dist_params <- c(0.1, 1e-2, 1)
+V_DE_ref_prior_dist_params <- c(0.04, 1e-3, 1)
+V_UE_ref_prior_dist_params <- c(0.005, 1e-4, 0.1)
 Ea_V_DE_prior_dist_params <- c(40, 5, 80)
 Ea_V_UE_prior_dist_params <- c(30, 5, 80)
-r_M_prior_dist_params <- c(0.0001667, 0, 0.1)
-r_E_prior_dist_params <- c(0.0002, 0, 0.1)
-r_L_prior_dist_params <- c(0.0004, 0, 0.1)
+r_M_prior_dist_params <- c(0.00016667, 1e-5, 0.1)
+r_E_prior_dist_params <- c(0.0002, 1e-5, 0.1)
+r_L_prior_dist_params <- c(0.0004, 1e-5, 0.1)
 
 #Create list of lists to pass prior means as initial theta values in Stan corresponding to four chains.
 init_theta_single = list(
